6f1e870815ef0ead08f34a8c7c3d814be6d473e6
bmoren <ben@benmoren.com>
2026-04-22 18:45:25 -0500

    add ross stuff

--- server.js
+++ server.js
@@ -12,28 +12,64 @@ const PORT = process.env.PORT || 3000;
 const PUBLIC_DIR = path.join(__dirname, 'public');
 const MEDIA_DIR = path.join(PUBLIC_DIR, 'media');
 
+const si = require("systeminformation");
+
+const { StillCamera } = require("pi-camera-connect");
+const stillCamera = new StillCamera();
+
+
 // Serve static files from /public
 app.use(express.static(PUBLIC_DIR));
 
-// Auto-route media files: GET /media/:filename
-// Returns a directory listing as JSON if no filename given
-app.get('/media', (req, res) => {
-  fs.readdir(MEDIA_DIR, (err, files) => {
-    if (err) return res.status(500).json({ error: 'Could not read media directory' });
-    res.json({ files });
+
+app.get('/cputemp', (req, res) => {
+  si.cpuTemperature()
+  .then((data) => {
+
+    res.json({'cpuTemprature': data})
+    console.log(data)
+  })
+  .catch((error) => {
+    res.status(500).json({ error: 'Could not read CPU Temp ' });
   });
+
 });
 
-app.get('/media/:filename', (req, res) => {
-  const filePath = path.join(MEDIA_DIR, path.basename(req.params.filename));
-  // Prevent directory traversal
-  if (!filePath.startsWith(MEDIA_DIR)) {
-    return res.status(403).send('Forbidden');
-  }
-  res.sendFile(filePath, (err) => {
-    if (err) res.status(404).send('File not found');
-  });
+app.get('/time', (req, res) => {
+    res.json({'time':  si.time() })
 });
+
+app.get('/signal', (req, res) => {
+si.wifiConnections()
+  .then((data) => {
+
+    res.json({'wifi': data})
+    console.log(data)
+  })
+  .catch((error) => {
+    res.status(500).json({ error: 'Could not read wifi Data ' });
+  });
+})
+
+
+app.get('/mic', (req, res) => {
+
+})
+
+app.get('/camera', (req, res) => {
+
+  stillCamera.takeImage().then(image => {
+
+    fs.writeFile("public/webcam/webcam.jpg", image, (err)=>{
+      if (err) throw err;
+      res.json({'image': "public/webcam/webcam.jpg"})
+    });
+});
+
+})
+
+
+
 
 // Socket.io
 io.on('connection', (socket) => {
@@ -44,6 +80,11 @@ io.on('connection', (socket) => {
     console.log(`[socket] message from ${socket.id}:`, data);
     socket.broadcast.emit('message', { from: socket.id, ...data });
   });
+
+  socket.on('addItem', (itemName) => {
+    
+
+  })
 
   // Example: send to a specific room
   socket.on('join', (room) => {
